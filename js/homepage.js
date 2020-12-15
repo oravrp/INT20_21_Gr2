74206010c61824d13dd301ceadbb4a57224f19b3
Ora Vrapcani <oravrapcani@gmail.com>
2020-12-15 23:07:14 +0100

    added some more functionalities using javascript

--- js/homepage.js
+++ js/homepage.js
@@ -2,6 +2,33 @@
  window.addEventListener("scroll", function () {
              var header = document.querySelector("header");
             header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
 
 /* A function to change the header-nav styling  while scrolling */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
