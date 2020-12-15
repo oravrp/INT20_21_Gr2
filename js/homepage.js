ce4a0a11d8f1fb6ef37d97ba49346e1b04b47947
Ora Vrapcani <oravrapcani@gmail.com>
2020-12-15 23:10:40 +0100

    code modularization

--- js/homepage.js
+++ js/homepage.js
@@ -1,11 +1,11 @@
 
+ /* A function to change the header-nav styling  while scrolling */
  window.addEventListener("scroll", function () {
              var header = document.querySelector("header");
             header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-/* A function to change the header-nav styling  while scrolling */
-
+/* Javascript code for the slider in the bg-color section*/
 var slideIndex = 1;
 showSlides(slideIndex);
 
