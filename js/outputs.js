69e2c3f8f97e946cf53f41d9c568f8aea09e0ac4
Lind Krasniqi <lind.krasniqi@student.uni-pr.edu>
2021-01-05 00:08:28 +0100

    Added the output page and the relevant files for css and Js

---
+++ js/outputs.js
@@ -0,0 +1,29 @@
+//Ordered lists animation on scroll
+const container = document.querySelector('.lists');
+observer = new IntersectionObserver ((entries)=> {
+console.log(entries);
+if (entries[0].intersectionRatio > 0) {
+  entries[0].target.style.animation = 'anim2 2s forwards ease-out';
+}
+else {
+  entries[0].target.style.animation = "none";
+}
+})
+observer.observe(container);
+
+//---------------------------------------------------//
+
+//eyeballs following mousemove function
+let eyeballs = document.querySelectorAll(".eye");
+document.onmousemove = function () {
+   let x = (event.clientX * 100) / window.innerWidth - 50 + "%";
+   let y = (event.clientY * 100) / window.innerHeight - 50 + "%";
+
+   for (let i = 0; i < eyeballs.length; i++) {
+       eyeballs[i] .style.transform = "translate(" + x + "," + y + ")";
+   }
+}
+//Change Text on click
+function changeText () {
+   document.getElementById("text").innerHTML = "Faleminderit p&eumlr koh&eumln tuaj, konsumator&eumlt jan&euml &ccedilelesi i suksesit ton&euml !";
+}
