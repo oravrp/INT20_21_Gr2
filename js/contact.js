9caa0a06e5a9c177eae691c383ee25e891e8027f
Lind Krasniqi <lind.krasniqi@student.uni-pr.edu>
2021-01-10 23:44:58 +0100

    Did some small changes

--- js/contact.js
+++ js/contact.js
@@ -18,50 +18,62 @@
   	form.addEventListener('submit', (e) =>{
   		let messages = []
   		if (emri.value === '' || emri.value == null){
-  			emriErr.style.visibility = "visible";
-  			messages.push("This field is required");
+			  emriErr.style.visibility = "visible";
+			  emri.style.border = "1px solid red";
+			messages.push("This field is required");
+			  
 		}
 		else if (emri.value.length >0 && isNaN(emri.value)){
 			emriErr.style.visibility = "hidden";
+			emri.style.border = "1px solid green";
 		} 
   		 else if (!isNaN(emri.value)) {
               emriErr.innerText = "You can not put numbers in this field";
               emriErr.style.color = "red";
-              emriErr.style.visibility = "visible";
+			  emriErr.style.visibility = "visible";
+			  emri.style.border = "1px solid red";
               messages.push("This field is required");
           }
   		if (mail.value === '' || mail.value == null) {
-  			mailErr.style.visibility = "visible";
+			  mailErr.style.visibility = "visible";
+			  mail.style.border = "1px solid red";
   			messages.push("This filed is required");
 		}
 		else if (mail.value.length > 0 ) {
 			mailErr.style.visibility = "hidden";
+			mail.style.border = "1px solid green";
 		}  
   		 else if (!ValidateEmail(mail.value)) {
               mailErr.innerText = "This email is invalid";
               mailErr.style.color = "red";
-              mailErr.style.visibility = "visible";
+			  mailErr.style.visibility = "visible";
+			  mail.style.border = "1px solid green";
               messages.push("This field is required");
           }
   		if (Subject.value === '' || Subject.value == null) {
-  			subjErr.style.visibility = "visible";
+			  subjErr.style.visibility = "visible";
+			  Subject.style.border = "1px solid red";
   			messages.push("This field is required");
 		}
 		else if (Subject.value.length>0) {
 			subjErr.style.visibility = "hidden";
+			Subject.style.border = "1px solid green";
 		}
 		  
   		if (txtArea.value === '' || txtArea.value==null){
-  			MessErr.style.visibility = "visible";
+			  MessErr.style.visibility = "visible";
+			  txtArea.style.border = "1px solid red";
   			messages.push("This field is required");
 		 }
 		else if (txtArea.value.length > 20 ) {
 			MessErr.style.visibility = "hidden";
+			txtArea.style.border = "1px solid green";
 		}  
   		else if (txtArea.value.length < 20) {
   			MessErr.innerText = "Your text should contain at least 20 characters";
   			MessErr.style.color = "red";
-  			MessErr.style.visibility = "visible";
+			  MessErr.style.visibility = "visible";
+			  txtArea.style.border = "1px solid red";
   			messages.push("This field is required");
 
 		  }
