9f29975b5d997016b366c808f839f37a835575f5
FlamurMustafa <flamurmustafa2001@gmail.com>
2021-01-13 07:15:30 +0100

    created ServiceWorker script file

---
+++ sw.js
@@ -0,0 +1,49 @@
+const cacheName = 'v1'
+
+const cacheAssets = [
+    './',
+    'staff.html',
+    '/css/staff.css',
+    '/js/staff.js',
+    '/css/navFooter.css'
+]
+
+
+//install event
+self.addEventListener('install', e => {
+    console.log("service worker installed")
+
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log("service worker caching Files")
+            cache.addAll(cacheAssets);
+        })
+
+    )
+})
+self.addEventListener('activate', e => {
+    console.log('service worker activated')
+        //removing old caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('service worker deleting old cache')
+                        return caches.delete(cache)
+                    }
+                })
+            )
+        })
+    );
+})
+
+//call fetch event
+self.addEventListener('fetch', e => {
+    console.log("service worker fetching")
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    )
+})
