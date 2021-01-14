cee92a03fba0ed27dfe0b40d75f5251b03efb179
Ora Vrapcani <oravrapcani@gmail.com>
2021-01-14 22:24:59 +0100

    used js constructors,added pacman game,nav last changes

--- js/case.js
+++ js/case.js
@@ -58,7 +58,38 @@ function kliko() {
 
 newRe = new RegExp('Drag')
 str1 = document.getElementById('check').innerHTML;
+
 // Check whether regular expression exists in the string.
 if (newRe.test(str1)) {
     alert("'Drag' is found in " + str1);
 }
+
+//Js Constructors
+function fillTable(s,c,pm,pc)
+{
+    this.country = s;
+    this.city = c;
+    this.pm10 = pm;
+    this.postalCode = pc;
+}
+fillTable("Kosov&euml;","Prishtin&euml;",86.7,10000);
+        var fields = document.getElementsByClassName("objectPurpose");
+        
+        fields[0].innerHTML = this.country;
+        fields[1].innerHTML = this.city;
+        fields[2].innerHTML = this.pm10;
+        fields[3].innerHTML = this.postalCode;
+function fillTable2(p1,p2,p3,p4)
+{
+    this.q1 = p1;
+    this.q2 = p2;
+    this.q3 = p3;
+    this.q4 = p4;
+}
+fillTable2('20%','30%','50%','40%');
+var fields2 = document.getElementsByClassName("objectPurpose1");
+        
+        fields2[0].innerHTML = this.q1;
+        fields2[1].innerHTML = this.q2;
+        fields2[2].innerHTML = this.q3;
+        fields2[3].innerHTML = this.q4;
---
+++ js/pacman.js
@@ -0,0 +1,105 @@
+//0 empty, 1 coin, 2 wall 3 cherry
+var world=[
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,1,1,0,1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,2],
+    [2,1,2,1,1,2,1,1,2,1,0,1,2,1,1,2,0,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,1,1,1,2],
+    [2,1,2,1,1,2,1,1,2,1,0,1,2,1,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,0,3,2,1,1,1,1,1,2],
+    [3,1,2,1,1,2,1,1,2,0,0,1,2,1,1,2,1,1,2,0,1,2,1,2,2,2,2,1,2,2,2,2,1,1,1,1,1,1],
+    [1,1,2,3,1,2,1,1,2,1,0,1,2,0,0,1,1,1,2,0,1,2,1,2,2,1,1,1,2,1,1,1,1,1,1,3,1,1],
+    [2,1,2,3,1,2,1,1,2,0,1,1,2,1,1,2,1,1,2,0,1,2,1,2,1,2,1,1,2,1,1,1,1,1,1,1,1,2],
+    [2,1,2,1,1,2,1,1,2,1,0,1,2,1,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,3,1,1,1,1,1,2],
+    [2,1,1,2,2,1,2,2,1,1,0,1,2,1,1,2,1,1,1,2,2,1,1,2,1,1,2,1,2,1,1,1,1,1,2,1,1,2],
+    [2,1,3,0,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [2,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,0,1,1,1,1,1,1,2],
+    [2,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,3,1,1,3,1,0,0,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+
+    ];
+    function displayWorld(){
+    var output = '';
+
+    for(var i=0; i<world.length; i++){
+        output += "\n<div class='row'>\n";
+        for(var j=0; j<world[i].length; j++){
+            if(world[i][j] == 2)
+                output += "<div class='brick'></div>";
+            else if(world[i][j] == 1)
+                output += "<div class='coin'></div>";
+            else if(world[i][j] == 3)
+                output += "<div class='cherry'></div>";
+            else if(world[i][j] == 0)
+                output += "<div class='empty'></div>";
+        }
+        output += "\n</div>";
+    }
+    // console.log(output);
+    document.getElementById('world').innerHTML = output;
+}
+
+var score = 0;
+
+var pacman = {
+    x: 1,
+    y: 1
+}
+
+function displayPacman(){
+    document.getElementById('pacman').style.top = pacman.y*24.5+"px";
+    document.getElementById('pacman').style.left = pacman.x*20+"px";
+}
+
+function displayScore(){
+    document.getElementById('score').innerHTML = score;
+}
+
+displayWorld();
+displayPacman();
+displayScore();
+
+document.onkeydown = function(e){
+    if(e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2 && pacman.x == 0){
+        document.getElementById('pacman').style.transform = "rotate(-180deg)";
+        pacman.x = world[0].length-1;
+    }
+    else if(e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2 && pacman.x > 0){
+        document.getElementById('pacman').style.transform = "rotate(-180deg)";
+        pacman.x--;
+    }
+    else if(e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2 && pacman.x == world[0].length-1){
+        document.getElementById('pacman').style.transform = "none";
+        pacman.x = 0;
+    }
+    else if(e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2){
+        document.getElementById('pacman').style.transform = "none";
+        pacman.x++;
+    }
+    else if(e.keyCode == 38 && world[pacman.y-1][pacman.x] != 2 && pacman.y > 0){
+        document.getElementById('pacman').style.transform = "rotate(-90deg)";
+        pacman.y--;
+    }
+    else if(e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2){
+        document.getElementById('pacman').style.transform = "rotate(90deg)";
+        pacman.y++;
+    }
+
+    if (world[pacman.y][pacman.x] == 1) {
+        world[pacman.y][pacman.x] = 0;
+        score+=10;
+        displayWorld();
+        displayScore();
+    }
+    else if (world[pacman.y][pacman.x] == 3) {
+        world[pacman.y][pacman.x] = 0;
+        score+=50;
+        displayWorld();
+        displayScore();
+    }
+    displayPacman();
+}
+if(score==4900){
+    alert("You Won!");
+}
