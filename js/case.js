7e0502054a1fc00a4db0cbd3159827eae7dbd3d2
Ora Vrapcani <oravrapcani@gmail.com>
2021-05-14 23:38:32 +0200

    added web api - weather forecast

--- js/case.js
+++ js/case.js
@@ -6,19 +6,6 @@ ctx.fillText("Lorem Ipsum", 10, 50);
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-// Fill listeners
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
-
-// Loop through empty boxes and add listeners
-for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-
-
 //Perdorimi i metodave Exec() and Test() tek RegEx
 function kliko() {
     var str = document.getElementById('check').innerHTML;
